5ad1168a7671e0bd71eeecff7ae08f6fbc6daf53
Thoby Jim Ralleta <152825042+thobidogs@users.noreply.github.com>
2025-03-07 17:24:32 +0800

    Update WasteManagementSystem.java

--- WasteManagementSystem.java
+++ WasteManagementSystem.java
@@ -2,7 +2,7 @@ public class WasteManagementSystem {
     private WasteCollector chain;
 
     public WasteManagementSystem() {
-        // Setting up the chain
+        
         this.chain = new OrganicWasteCollector();
         WasteCollector c2 = new RecyclableWasteCollector();
         WasteCollector c3 = new HazardousWasteCollector();
